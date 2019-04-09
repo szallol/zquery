7c5e194b8601e9e1f55cfd026b4a5b19c4b85f1f
Szallo Laszlo <szallol@gmail.com>
2019-04-09 16:14:01 +0300

    - set up minimal clap

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,22 @@
 extern crate clap;
 extern crate zquery;
 
-use clap::App;
+use clap::{Arg, App, SubCommand};
 use zquery::ZQuery;
 
 fn main() {
- App::new("zq")
-       .version("0.1.0")
-      .about("command line query tool(SQL) for differrent data sources and destinations")
-       .author("Szallo Laszlo")
-       .get_matches();
+ let matches = App::new("zq")
+     .version("0.1.0")
+     .about("command line query tool(SQL) for different data sources and destinations")
+     .author("Szallo L. <szallol@gmail.com>")
+     .arg(Arg::with_name("input")
+         .short("i")
+         .long("input")
+         .value_name("INPUT")
+         .help("Input source")
+         .multiple(true)
+         .takes_value(true))
+     .get_matches();
+
+
 }
